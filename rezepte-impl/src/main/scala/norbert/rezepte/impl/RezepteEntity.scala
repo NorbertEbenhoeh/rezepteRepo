6dba71a7ee509321b9ca85552201f3e308ac2100
NorbertEbenhoeh <dernobbi@gmx.de>
2017-11-05 16:27:44 +0100

    forgot to add the other commits

--- rezepte-impl/src/main/scala/norbert/rezepte/impl/RezepteEntity.scala
+++ rezepte-impl/src/main/scala/norbert/rezepte/impl/RezepteEntity.scala
@@ -11,25 +11,6 @@ import com.lightbend.lagom.scaladsl.playjson.{JsonSerializer, JsonSerializerRegi
 import norbert.rezepte.model.Rezept
 import play.api.libs.json._
 
-/**
-  * This is an event sourced entity. It has a state, [[RezepteState]], which
-  * stores what the greeting should be (eg, "Hello").
-  *
-  * Event sourced entities are interacted with by sending them commands. This
-  * entity supports two commands, a [[UseGreetingMessage]] command, which is
-  * used to change the greeting, and a [[Hello]] command, which is a read
-  * only command which returns a greeting to the name specified by the command.
-  *
-  * Commands get translated to events, and it's the events that get persisted by
-  * the entity. Each event will have an event handler registered for it, and an
-  * event handler simply applies an event to the current state. This will be done
-  * when the event is first created, and it will also be done when the entity is
-  * loaded from the database - each event will be replayed to recreate the state
-  * of the entity.
-  *
-  * This entity defines one event, the [[GreetingMessageChangedOrRezeptAdded]] event,
-  * which is emitted when a [[UseGreetingMessage]] command is received.
-  */
 class RezepteEntity extends PersistentEntity {
 
   override type Command = RezepteCommand[_]
@@ -39,47 +20,29 @@ class RezepteEntity extends PersistentEntity {
   /**
     * The initial state. This is used if there is no snapshotted state to be found.
     */
-  override def initialState: RezepteState = RezepteState("Hello", LocalDateTime.now.toString, Rezept(1, "Sehr sehr lecker", 3, public = true))
+  override def initialState: RezepteState = RezepteState(LocalDateTime.now.toString, Vector.empty[Rezept])
 
   /**
     * An entity can define different behaviours for different states, so the behaviour
     * is a function of the current state to a set of actions.
     */
   override def behavior: Behavior = {
-    case RezepteState(message, _, rezept) => Actions()
-      .onCommand[UseGreetingMessage, Done] {
-
-      // Command handler for the UseGreetingMessage command
-      case (UseGreetingMessage(newMessage), ctx, state) =>
-        // In response to this command, we want to first persist it as a
-        // GreetingMessageChanged event
-        ctx.thenPersist(
-          GreetingMessageChangedOrRezeptAdded(newMessage, state.rezept, rezeptAdded = false)) { _ =>
-          // Then once the event is successfully persisted, we respond with done.
-          ctx.reply(Done)
-        }
-    }.onCommand[AddRezept, Done]{
+    case RezepteState(_, rezepte) => Actions()
+      .onCommand[AddRezept, Done]{
 
       case (AddRezept(newRezept), ctx, state) =>
-
         ctx.thenPersist(
-          GreetingMessageChangedOrRezeptAdded(state.message, newRezept, rezeptAdded = true)) {_ => ctx.reply(Done) }
-    }.onReadOnlyCommand[Hello, String] {
+          RezeptAdded(newRezept)) { _ => ctx.reply(Done) }
+    }.onReadOnlyCommand[GetRezepte, Vector[Rezept]] {
 
-      // Command handler for the Hello command
-      case (Hello(name), ctx, state) =>
-        // Reply with a message built from the current message, and the name of
-        // the person we're meant to say hello to.
-        ctx.reply(s"$message, $name! The state is $state")
+      case (GetRezepte(bla), ctx, state) =>
+        // Reply with all rezepte from the state
+        ctx.reply(state.rezepte)
     }.onEvent {
 
-      // Event handler for the GreetingMessageChanged event
-      case (GreetingMessageChangedOrRezeptAdded(newMessage, _, rezeptAdded), state) if rezeptAdded =>
-        // We simply update the current state to use the greeting message from
-        // the event.
-        RezepteState(newMessage, LocalDateTime.now().toString, state.rezept)
-      case (GreetingMessageChangedOrRezeptAdded(_, newRezept, rezeptAdded), state) if rezeptAdded =>
-        RezepteState(state.message, LocalDateTime.now().toString, newRezept)
+      // Event handler for the RezeptAdded event
+      case (RezeptAdded(newRezept), state) =>
+        RezepteState(LocalDateTime.now().toString, state.rezepte :+ newRezept)
     }
   }
 }
@@ -87,7 +50,7 @@ class RezepteEntity extends PersistentEntity {
 /**
   * The current state held by the persistent entity.
   */
-case class RezepteState(message: String, timestamp: String, rezept: Rezept)
+case class RezepteState(timestamp: String, rezepte: Vector[Rezept])
 
 object RezepteState {
   /**
@@ -116,9 +79,9 @@ object RezepteEvent {
 /**
   * An event that represents a change in greeting message.
   */
-case class GreetingMessageChangedOrRezeptAdded(message: String, rezept: Rezept, rezeptAdded: Boolean) extends RezepteEvent
+case class RezeptAdded(rezepte: Rezept) extends RezepteEvent
 
-object GreetingMessageChangedOrRezeptAdded {
+object RezeptAdded {
 
   /**
     * Format for the greeting message changed event.
@@ -126,7 +89,7 @@ object GreetingMessageChangedOrRezeptAdded {
     * Events get stored and loaded from the database, hence a JSON format
     * needs to be declared so that they can be serialized and deserialized.
     */
-  implicit val format: Format[GreetingMessageChangedOrRezeptAdded] = Json.format
+  implicit val format: Format[RezeptAdded] = Json.format
 }
 
 /**
@@ -134,55 +97,19 @@ object GreetingMessageChangedOrRezeptAdded {
   */
 sealed trait RezepteCommand[R] extends ReplyType[R]
 
-/**
-  * A command to switch the greeting message.
-  *
-  * It has a reply type of [[Done]], which is sent back to the caller
-  * when all the events emitted by this command are successfully persisted.
-  */
-case class UseGreetingMessage(message: String) extends RezepteCommand[Done]
-
-object UseGreetingMessage {
-
-  /**
-    * Format for the use greeting message command.
-    *
-    * Persistent entities get sharded across the cluster. This means commands
-    * may be sent over the network to the node where the entity lives if the
-    * entity is not on the same node that the command was issued from. To do
-    * that, a JSON format needs to be declared so the command can be serialized
-    * and deserialized.
-    */
-  implicit val format: Format[UseGreetingMessage] = Json.format
-}
-
-/**
-  * A command to say hello to someone using the current greeting message.
-  *
-  * The reply type is String, and will contain the message to say to that
-  * person.
-  */
-case class Hello(name: String) extends RezepteCommand[String]
-
-object Hello {
-
-  /**
-    * Format for the hello command.
-    *
-    * Persistent entities get sharded across the cluster. This means commands
-    * may be sent over the network to the node where the entity lives if the
-    * entity is not on the same node that the command was issued from. To do
-    * that, a JSON format needs to be declared so the command can be serialized
-    * and deserialized.
-    */
-  implicit val format: Format[Hello] = Json.format
-}
 
 case class AddRezept(rezept: Rezept) extends RezepteCommand[Done]
 
 object AddRezept {
 
   implicit val format: Format[AddRezept] = Json.format
+}
+
+case class GetRezepte(name: String) extends RezepteCommand[Vector[Rezept]]
+
+object GetRezepte {
+
+  implicit val format: Format[GetRezepte] = Json.format
 }
 
 /**
@@ -196,11 +123,10 @@ object AddRezept {
   */
 object RezepteSerializerRegistry extends JsonSerializerRegistry {
   override def serializers: Seq[JsonSerializer[_]] = Seq(
-    JsonSerializer[UseGreetingMessage],
-    JsonSerializer[Hello],
     JsonSerializer[Rezept],
     JsonSerializer[AddRezept],
-    JsonSerializer[GreetingMessageChangedOrRezeptAdded],
+    JsonSerializer[GetRezepte],
+    JsonSerializer[RezeptAdded],
     JsonSerializer[RezepteState]
   )
 }
--- rezepte-impl/src/main/scala/norbert/rezepte/impl/RezepteServiceImpl.scala
+++ rezepte-impl/src/main/scala/norbert/rezepte/impl/RezepteServiceImpl.scala
@@ -4,56 +4,38 @@ import com.lightbend.lagom.scaladsl.api.ServiceCall
 import com.lightbend.lagom.scaladsl.api.broker.Topic
 import com.lightbend.lagom.scaladsl.broker.TopicProducer
 import com.lightbend.lagom.scaladsl.persistence.{EventStreamElement, PersistentEntityRegistry}
-import norbert.rezepte.api
 import norbert.rezepte.api.RezepteService
+import norbert.rezepte.model._
 
 /**
   * Implementation of the RezepteService.
   */
 class RezepteServiceImpl(persistentEntityRegistry: PersistentEntityRegistry) extends RezepteService {
 
-  override def hello(id: String) = ServiceCall { _ =>
-    // Look up the rezepte entity for the given ID.
-    val ref = persistentEntityRegistry.refFor[RezepteEntity](id)
 
-    // Ask the entity the Hello command.
-    ref.ask(Hello(id))
-  }
-
-  override def useGreeting(id: String) = ServiceCall { request =>
-    // Look up the rezepte entity for the given ID.
-    val ref = persistentEntityRegistry.refFor[RezepteEntity](id)
-
-    // Tell the entity to use the greeting message specified.
-    ref.ask(UseGreetingMessage(request.message))
-  }
-
-
-  override def greetingsTopic(): Topic[api.GreetingMessageChanged] =
+  override def rezepteTopic(): Topic[RezeptCreated] =
     TopicProducer.singleStreamWithOffset {
       fromOffset =>
         persistentEntityRegistry.eventStream(RezepteEvent.Tag, fromOffset)
           .map(ev => (convertEvent(ev), ev.offset))
     }
 
-  private def convertEvent(helloEvent: EventStreamElement[RezepteEvent]) = {
-    helloEvent.event match {
-      case GreetingMessageChangedOrRezeptAdded(msg, rezept, rezeptAdded) => api.GreetingMessageChanged(helloEvent.entityId, msg, rezept, rezeptAdded)
-//      case RezeptAdded(rezept) => api.RezeptAdded(Rezept(rezept.rezeptId, rezept.text, rezept.rating, rezept.public))
+  private def convertEvent(rezeptEvent: EventStreamElement[RezepteEvent]) = {
+    rezeptEvent.event match {
+      case RezeptAdded(rezept) => RezeptCreated(rezept)
     }
   }
 
 
   override def getRezepte = ServiceCall { _ =>
-    ???
-//    // Look up the rezepte entity
-//    vel ref = persistentEntityRegistry.refFor[RezepteEntity]()
-//
-//    ref.ask
+    // Look up the rezepte entity
+    val ref = persistentEntityRegistry.refFor[RezepteEntity]("rezepte")
+
+    ref.ask(GetRezepte("rezept"))
   }
 
   override def postRezepte = ServiceCall { rezept =>
-    val ref = persistentEntityRegistry.refFor[RezepteEntity](rezept.rezeptId.toString)
+    val ref = persistentEntityRegistry.refFor[RezepteEntity]("rezepte")
 
     ref.ask(AddRezept(rezept))
   }
